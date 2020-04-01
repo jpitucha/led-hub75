243b0b2287c680bfadd089eaeec8fc6790366080
Jakub Pitucha <vaguevans@gmail.com>
2020-04-01 02:48:46 +0200

    Project added

---
+++ src/main.cpp
@@ -0,0 +1,23 @@
+#include <Arduino.h>
+
+#define A
+#define B
+#define C
+#define D
+#define R0
+#define R1
+#define G0
+#define G1
+#define B0
+#define B1
+#define CLK
+#define LATCH
+#define OE
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
