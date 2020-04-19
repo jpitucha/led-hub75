b287291f4fa93e52c73194f78d28b0cdce7eb237
Jakub Pitucha <vaguevans@gmail.com>
2020-04-19 18:07:57 +0200

    removed arduino project file, python added

---
+++ led-hub75.py
@@ -0,0 +1,18 @@
+from gpiozero import OutputDevice
+
+A = OutputDevice()
+B = OutputDevice()
+C = OutputDevice()
+D = OutputDevice()
+R1 = OutputDevice()
+R2 = OutputDevice()
+G1 = OutputDevice()
+G2 = OutputDevice()
+B1 = OutputDevice()
+B2 = OutputDevice()
+CLK = OutputDevice()
+LATCH = OutputDevice()
+OE = OutputDevice()
+
+BLANK_DELAY_TIME_US = 5000
+
